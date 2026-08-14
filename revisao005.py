8eb1ab299f893b376cf3445e2e31761b707101d4
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-14 17:07:45 -0300

    Atualiza projeto após primeira versão

--- revisao005.py
+++ revisao005.py
@@ -1,6 +1,8 @@
 maisvelho = 0
-idade = 0
 for c in range(1, 5):
     nome = input("Digite seu nome: ")
     idade = int(input(f"Qual a idade de {nome}? "))
-    sexo = input("Qual o sexo? [M/F] ").strip().upper() [0]
+    if idade > maisvelho:
+        maisvelho = idade
+    sexo = input("Qual o sexo? [M/F] ").strip().upper() [0]    
+print(f'A pessoa mais velha entrevistada tem {maisvelho} anos.')
