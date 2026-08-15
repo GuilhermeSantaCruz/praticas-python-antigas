4b1ec0de255d59eb4a931fdceaeb90e8c7de15c6
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-15 08:19:40 -0300

    Adiciona nome da pessoa mais velha

--- revisao005.py
+++ revisao005.py
@@ -1,8 +1,10 @@
 maisvelho = 0
+nomemaisvelho = ""
 for c in range(1, 5):
     nome = input("Digite seu nome: ")
     idade = int(input(f"Qual a idade de {nome}? "))
     if idade > maisvelho:
         maisvelho = idade
+        nomemaisvelho = nome
     sexo = input("Qual o sexo? [M/F] ").strip().upper() [0]    
-print(f'A pessoa mais velha entrevistada tem {maisvelho} anos.')
+print(f'E a pessoa mais velha entrevistada foi {nomemaisvelho} com {maisvelho} anos.')
