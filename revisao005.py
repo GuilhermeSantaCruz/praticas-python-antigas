fe48b7ba2224db907518f1c7cb2f7fc2a60cb726
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-18 19:19:20 -0300

    Adiciona contagem de mulheres e aumenta entrevistas

--- revisao005.py
+++ revisao005.py
@@ -1,7 +1,8 @@
 maisvelho = 0
 tothomens = 0
+totmulheres = 0
 nomemaisvelho = ""
-for c in range(1, 5):
+for c in range(1, 8):
     nome = input("Digite seu nome: ")
     idade = int(input(f"Qual a idade de {nome}? "))
     if idade > maisvelho:
@@ -11,6 +12,9 @@ for c in range(1, 5):
     while sexo not in "MF":
         sexo = input("Qual o sexo? [M/F] ").strip().upper()[0] 
     if sexo == "M":
-        tothomens += 1           
+        tothomens += 1
+    if sexo == "F":
+        totmulheres += 1               
 print(f'E a pessoa mais velha entrevistada foi {nomemaisvelho} com {maisvelho} anos.')
 print(f'Tivemos um total de {tothomens} homens entrevistados.')
+print(f'Tivemos um total de {totmulheres} mulheres entrevistadas.')
