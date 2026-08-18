5897e282d5c17228aafe231fc27956a660be1fbc
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-17 21:38:08 -0300

    Adiciona contagem de homens entrevistados

--- revisao005.py
+++ revisao005.py
@@ -1,4 +1,5 @@
 maisvelho = 0
+tothomens = 0
 nomemaisvelho = ""
 for c in range(1, 5):
     nome = input("Digite seu nome: ")
@@ -8,5 +9,8 @@ for c in range(1, 5):
         nomemaisvelho = nome
     sexo = input("Qual o sexo? [M/F] ").strip().upper()[0]
     while sexo not in "MF":
-        sexo = input("Qual o sexo? [M/F] ").strip().upper()[0]        
+        sexo = input("Qual o sexo? [M/F] ").strip().upper()[0] 
+    if sexo == "M":
+        tothomens += 1           
 print(f'E a pessoa mais velha entrevistada foi {nomemaisvelho} com {maisvelho} anos.')
+print(f'Tivemos um total de {tothomens} homens entrevistados.')
