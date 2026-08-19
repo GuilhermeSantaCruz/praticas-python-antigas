69e88f30e1f7bff114d83ccaaf6c1b8f327ea381
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-19 20:13:23 -0300

    Adiciona identificação da pessoa mais nova

--- revisao005.py
+++ revisao005.py
@@ -1,20 +1,28 @@
-maisvelho = 0
 tothomens = 0
 totmulheres = 0
 nomemaisvelho = ""
+nomemaisnovo = ""
 for c in range(1, 8):
     nome = input("Digite seu nome: ")
     idade = int(input(f"Qual a idade de {nome}? "))
-    if idade > maisvelho:
-        maisvelho = idade
-        nomemaisvelho = nome
+    if c == 1:
+        maisvelho = maisnovo = idade
+        nomemaisnovo = nomemaisvelho = nome
+    else:
+        if idade < maisnovo:
+            maisnovo = idade    
+            nomemaisnovo = nome
+        if idade > maisvelho:
+            maisvelho = idade
+            nomemaisvelho = nome    
     sexo = input("Qual o sexo? [M/F] ").strip().upper()[0]
     while sexo not in "MF":
         sexo = input("Qual o sexo? [M/F] ").strip().upper()[0] 
     if sexo == "M":
         tothomens += 1
     if sexo == "F":
-        totmulheres += 1               
+        totmulheres += 1   
+print(f'E a pessoa mais nova entrevistada foi {nomemaisnovo} com {maisnovo} anos.')                    
 print(f'E a pessoa mais velha entrevistada foi {nomemaisvelho} com {maisvelho} anos.')
 print(f'Tivemos um total de {tothomens} homens entrevistados.')
 print(f'Tivemos um total de {totmulheres} mulheres entrevistadas.')
