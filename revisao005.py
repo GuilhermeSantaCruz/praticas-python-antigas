6bc2e8b7b28854b9152a489da5719c4cd6ffc2eb
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-20 08:44:35 -0300

    Adiciona cálculo da idade média dos entrevistados

--- revisao005.py
+++ revisao005.py
@@ -2,9 +2,11 @@ tothomens = 0
 totmulheres = 0
 nomemaisvelho = ""
 nomemaisnovo = ""
+somaidade = 0
 for c in range(1, 8):
     nome = input("Digite seu nome: ")
     idade = int(input(f"Qual a idade de {nome}? "))
+    somaidade += idade
     if c == 1:
         maisvelho = maisnovo = idade
         nomemaisnovo = nomemaisvelho = nome
@@ -22,7 +24,10 @@ for c in range(1, 8):
         tothomens += 1
     if sexo == "F":
         totmulheres += 1   
+mediaidade = somaidade / 7        
 print(f'E a pessoa mais nova entrevistada foi {nomemaisnovo} com {maisnovo} anos.')                    
 print(f'E a pessoa mais velha entrevistada foi {nomemaisvelho} com {maisvelho} anos.')
 print(f'Tivemos um total de {tothomens} homens entrevistados.')
 print(f'Tivemos um total de {totmulheres} mulheres entrevistadas.')
+print(f'A idade média das pessoas entrevistadas é de {mediaidade:.1f} anos.')
+
