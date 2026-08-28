56339fd17db4799000e42bf22accebeb46106350
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-27 21:05:21 -0300

    Adiciona times que estão na zona de classificação para a libertadores

--- revisao007.py
+++ revisao007.py
@@ -3,10 +3,7 @@ classificação = ("Flamengo", "Cruzeiro", "Palmeiras", "Mirassol",
                  "Bragantino", "Vasco da Gama", "Corinthians",
                  "Grêmio", "Ceará", "Atlético MG", "Internacional",
                  "Santos", "Juventude", "Vitória", "Fortaleza", "Sport")
-print(f"Os 5 primeiros colocados {classificação[:5]}")
 print(f"Os times em ordem alfabética: {sorted(classificação)}")
 print(f"O Ceará está na {classificação.index('Ceará')+1}ª posição ")
-print(f'os times que estão na zona de rebaixamento é {classificação[-4:]}')
-
-
-
+print(f"Os times que estão se classificando para a libertadores são {classificação[:5]}")
+print(f'os times que estão na zona de rebaixamento são {classificação[-4:]}')
