272ff7c5488c93f6c9f49f2a55d456485d65522d
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-08-30 20:40:40 -0300

    Adiciona validação e ajusta cálculo da média

--- revisao004.py
+++ revisao004.py
@@ -6,8 +6,10 @@ while resp != "N":
     n = int(input("Digite um número: "))
     c += 1
     soma += n
-    media = soma / c
-    resp = input("Quer continuar? [S/N] ").strip().upper() [0]
+    resp = input("Quer continuar? [S/N] ").strip().upper()[0]
+    while resp not in "SN":
+        resp = input("Quer continuar? [S/N] ").strip().upper()[0]
+media = soma / c        
 print(f"Você digitou {c} números ")
 print(f"A soma de todos os números é {soma}")  
 print(f"E a média é {media} ")
