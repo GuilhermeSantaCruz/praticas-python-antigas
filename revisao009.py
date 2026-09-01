fc1cdfcb08658d0727743c58c499a852a55c8e79
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-01 19:17:54 -0300

    Adiciona soma dos números da tupla num

--- revisao009.py
+++ revisao009.py
@@ -4,6 +4,7 @@ num = (int(input("Digite um número: ")),
        int(input("Digite o último número: ")))
 print(num, end=" ")
 print(f"\nO número 9 apareceu {num.count(9)} vezes ")
+print(f'A soma dos valores é {sum(num)}')
 if 3 in num:
        print(f"O número 3 apareceu ma {num.index(3)+1}ª posição. ")
 else:
@@ -12,4 +13,3 @@ print("Os números pares digitados foram: ", end="")
 for n in num:
        if n % 2 == 0:
               print(n, end=" ")
-
