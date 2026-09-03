d2586b5da13105b44b8f7e66065bb852f3096efd
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-03 20:42:25 -0300

    Adiciona menor valor da tupla num

--- revisao009.py
+++ revisao009.py
@@ -5,9 +5,10 @@ num = (int(input("Digite um número: ")),
 print(num, end=" ")
 print(f"\nO número 9 apareceu {num.count(9)} vezes ")
 print(f'A soma dos valores é {sum(num)}')
-print(f'O maior número da lista num é {max(num)}')
+print(f'O maior número da tupla num é {max(num)}')
+print(f'O menor número da tupla num é {min(num)}')
 if 3 in num:
-       print(f"O número 3 apareceu ma {num.index(3)+1}ª posição. ")
+       print(f"O número 3 apareceu na {num.index(3)+1}ª posição. ")
 else:
        print("O número 3 não apareceu em nenhuma posição. ")       
 print("Os números pares digitados foram: ", end="")
