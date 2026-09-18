2ae5f0b105d5b08d36f8808d6d3599a035324ff5
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-18 16:56:26 -0300

    Adiciona nome do entrevistado

--- revisao001.py
+++ revisao001.py
@@ -1,5 +1,7 @@
 sexo = "g"
+nome = input("Qual é o seu nome? ")
 while sexo not in "MmFf":
     sexo = input("Qual o seu sexo? ").strip().upper()[0]
+print(f"{nome} é do sexo {sexo}.")        
 print("Fim do programa. ")
 
