14d19c7388567dd8791cde8fc81fdd5c86f778c9
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-20 15:27:57 -0300

    Adiciona contagem de números pares e ímpares

--- revisao010.py
+++ revisao010.py
@@ -1,9 +1,16 @@
 num = []
 contmaiorX = 0
+contpar = 0
+contimpar = 0
 for n in range(1, 6):
     numero = int(input(f"Digite o {n}º valor: "))
     num.append(numero)
-    
+    if numero % 2 == 0:
+        contpar += 1
+    else:
+        contimpar += 1    
+print(f"Na lista num tem {contpar} valores PARES!") 
+print(f'Na lista num tem {contimpar} valores ÍMPARES')   
 print(f"Os números digitados foram {num}")
 for v in num:
     if v > 10:
