45b8b965f7ede5e58a89b58b623197a0cc91cb95
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-24 21:32:32 -0300

    Adiciona nascimento do entrevistado

--- revisao001.py
+++ revisao001.py
@@ -1,7 +1,8 @@
 sexo = "g"
 nome = input("Qual é o seu nome? ")
+nascimento = int(input("Em que ano você nasceu? "))
 while sexo not in "MmFf":
     sexo = input("Qual o seu sexo? ").strip().upper()[0]
-print(f"{nome} é do sexo {sexo}.")        
+print(f"{nome} nasceu no ano de {nascimento} e o seu sexo é {sexo}.")
 print("Fim do programa. ")
 
