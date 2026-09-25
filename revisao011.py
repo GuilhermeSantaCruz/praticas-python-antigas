324e8d37df4a5c508f74f172415f57d2a4f62e21
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2026-09-25 17:48:43 -0300

    Adiciona revisao011

---
+++ revisao011.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um valor: '))
+dobro = num * 2
+triplo = num * 3
+raizq = num ** 0.5
+print(f'O dobro de {num} é {dobro}')
+print(f'O triplo de {num} é {triplo}')
+print(f'A raiz quadrada de {num} é {raizq}')
